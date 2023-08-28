fe210e5f4750a07de06de23195ae193defba64d4
ayesha8221 <ayesha.glnt012@gmail.com>
2023-08-28 15:42:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <NavbarComp/>
+  <router-view/>
+  <FooterComp/>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavbarComp from './components/NavbarComp.vue';
+import FooterComp from './components/FooterComp.vue';
+
+export default {
+  components: {
+    NavbarComp, FooterComp
+  }
+}
+</script>
+
+
+<style>
+#app{
+
+  body{
+  height: 100vh;
+  width: 100%;
+}
+}
+
+
+
+</style>
---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,65 @@
+<template lang="">
+<nav>
+          <a class="navbar-brand" href="#">
+     
+    </a>  
+        
+<div class="nav d-flex justify-content-center align-items-center">
+    <img src="https://i.postimg.cc/vmZb6BwS/23-removebg-preview.png" alt="Bootstrap" width="80" height="" class="logo">
+    <a class="nav-link active" aria-current="page" href="#">Home</a>
+  
+  
+    <a class="nav-link" href="#">About Us</a>
+  
+  
+    <a class="nav-link" href="#">Products</a>
+  
+  
+    <a class="nav-link" href="#">Contact</a>
+  
+  
+    <a class="nav-link" href="#">Admin</a>
+  
+  
+    <a class="nav-link" aria-disabled="true"></a>
+
+</div>
+</nav>
+
+</template>
+<script>
+export default {
+
+
+}
+
+
+
+
+
+</script>
+<style scoped>
+.nav{
+    height: 80px;
+    width : auto;
+    background-color: #60350d;
+}
+
+.li {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.img.logo {
+    padding-right: 100px;
+}
+
+.nav-link {
+    color: #995C23;
+    font-size: 20px;
+    font-weight: bold;
+    -webkit-text-stroke: 0.2px #E4C2A2;
+}
+    
+</style>

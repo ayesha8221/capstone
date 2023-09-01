f8b81a4f27757d691b902bc30e1b3b2a3af6e5af
Ayesha Galant <ayesha.glnt012@gmail.com>
2023-09-01 10:48:06 +0200

    add cart

--- api/controllers/cart.js
+++ api/controllers/cart.js
@@ -12,16 +12,46 @@ const showCart = (req, res) => {
 
 //adding to cart
 // Create New Product
+// const createCart = (req, res) => {
+//     const id = req.body.userID; req.body.productID; req.body.amount;
+//     insertCart(id, (err, results) => {
+//         if (err){
+//             res.send(err);
+//         }else{
+//             res.json(results);
+//         }
+//     });
+// }
+
+// const createCart = (req, res) => {
+//     // Extract data from the request body
+  
+//     // Call the insertCart function with extracted data
+//     insertCart(req.params.id, (err, results) => {
+//       if (err) {
+//         res.send(err);
+//       } else {
+//         res.json(results);
+//       }
+//     });
+//   };
+
 const createCart = (req, res) => {
-    const id = req.body.userID; req.body.productID; req.body.amount;
-    insertCart(id, (err, results) => {
-        if (err){
-            res.send(err);
-        }else{
-            res.json(results);
-        }
+    const userID = req.params.id;
+    
+    // Assuming req.body contains the necessary data (e.g., prodID)
+    const { prodID } = req.body;
+  
+    // Call the insertCart function with the userID
+    insertCart(userID, prodID, (err, results) => {
+      if (err) {
+        res.status(500).json({ error: 'Database error' });
+      } else {
+        res.status(200).json({ message: 'Cart created successfully', cart: results });
+      }
     });
-}
+  };
+
 
 module.exports = {
     showCart,
--- api/models/cartModels.js
+++ api/models/cartModels.js
@@ -18,16 +18,70 @@ const getCart = (id, result,) => {
 
 //add something to cart
 
-const insertCart = (id, result) => {
-    db.query("INSERT INTO Cart (userID, prodID, Products.amount) VALUES (?, ?, ?) ;", [id], (err, results) => {             
-        if(err) {
-            console.log('Database error');
-            result(err, null);
+const insertCart = (userID, callback) => {
+    db.query(
+      'INSERT INTO Cart (userID, prodID, amount) SELECT ? AS userID, Products.prodID, Products.amount FROM Products WHERE Cart.userID = ?;',
+      [userID, userID],
+      (err, results) => {
+        if (err) {
+          console.log('Database error');
+          callback(err, null);
         } else {
-            result(null, results);
+          callback(null, results);
         }
-    });   
-}
+      }
+    );
+  };
+
+// const insertCart = (id, result) => {
+//   db.query(
+//     "INSERT INTO Cart.cartID SET Products.prodName AS prodName, Products.amount FROM Cart JOIN Products ON Cart.prodID = Products.prodID WHERE Cart.userID = ?",
+//     [id],
+//     (err, results) => {
+//       if (err) {
+//         console.log('Database error');
+//         result(err, null);
+//       } else {
+//         result(null, results);
+//       }
+//     }
+//   );
+// };
+
+// const insertCart = (userID, productID, amount, callback) => {
+//     // Fetch the "amount" from the "Products" table based on the "productID"
+//     db.query(
+//       "SELECT amount FROM Products WHERE prodID = ?",
+//       [productID],
+//       (err, productResult) => {
+//         if (err) {
+//           console.log('Database error:', err);
+//           callback(err, null);
+//         } else {
+//           if (productResult.length === 0) {
+//             // Handle the case where the product with the given ID doesn't exist
+//             callback("Product not found", null);
+//           } else {
+//             const cartAmount = productResult[0].amount;
+  
+//             // Insert the item into the cart with the retrieved "amount"
+//             db.query(
+//               "INSERT INTO Cart (userID, prodID, amount) VALUES (?, ?, ?)",
+//               [userID, productID, amount],
+//               (err, insertResult) => {
+//                 if (err) {
+//                   console.log('Database error:', err);
+//                   callback(err, null);
+//                 } else {
+//                   callback(null, insertResult);
+//                 }
+//               }
+//             );
+//           }
+//         }
+//       }
+//     );
+//   };
 
 module.exports = {
     getCart,
